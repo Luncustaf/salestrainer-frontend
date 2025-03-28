3a61cad1cc27a3aa44b09609327d7f383a4b7e90
Simon Diegmann <simondiegmann@gmail.com>
2025-03-28 10:38:00 +0100

    Hooks ts

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,10 +1,16 @@
 // src/hooks.server.ts
 
 import { createServerClient, type CookieOptions } from '@supabase/ssr';
+import { dev } from '$app/environment';
 import type { Handle } from '@sveltejs/kit';
 import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
 
 export const handle: Handle = async ({ event, resolve }) => {
+  const cookieOptions: CookieOptions = {
+    path: '/',
+    secure: !dev
+  };
+
   event.locals.supabase = createServerClient(
     PUBLIC_SUPABASE_URL,
     PUBLIC_SUPABASE_ANON_KEY,
@@ -14,14 +20,15 @@ export const handle: Handle = async ({ event, resolve }) => {
           return event.cookies.get(name);
         },
         set(name, value, options) {
-          event.cookies.set(name, value, options as CookieOptions);
+          event.cookies.set(name, value, { ...cookieOptions, ...options });
         },
         remove(name, options) {
-          event.cookies.delete(name, options as CookieOptions);
+          event.cookies.delete(name, { ...cookieOptions, ...options });
         }
       }
     }
   );
 
-  return resolve(event);
+  const response = await resolve(event);
+  return response;
 };
