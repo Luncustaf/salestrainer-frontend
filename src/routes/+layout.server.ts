698f5dbc78ce7b83f3f9e920f27c2841349801e1
Simon Diegmann <simondiegmann@gmail.com>
2025-03-28 10:46:53 +0100

    Hooks ts

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -4,7 +4,9 @@ import { redirect } from '@sveltejs/kit';
 
 export const load: LayoutServerLoad = async ({ locals, url }) => {
   const session = locals.session;
-
+  console.log('🔍 Session in layout.server.ts:', session);
+  console.log('🔍 Aktueller Pfad:', url.pathname);
+  
   // Wenn keine Session vorhanden → redirect zur Login-Seite
   if (!session && !['/login', '/auth-success'].includes(url.pathname)) {
     throw redirect(303, '/login');
